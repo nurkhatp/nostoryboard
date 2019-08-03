77d3f55c4bac5fb88c87cf0635fa2887cc6eb7fe
nurkhat <npazylov@wakeapp.ru>
2019-08-03 09:03:35 +0300

    Init

---
+++ Readme.md
@@ -0,0 +1,2 @@
+# Project app without a storyboard
+
---
+++ WithoutStoryboard/HomeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HomeViewController.swift
+//  WithoutStoryboard
+//
+//  Created by Nurkhat Pazylov on 8/3/19.
+//  Copyright © 2019 Storyboard. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.red
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
